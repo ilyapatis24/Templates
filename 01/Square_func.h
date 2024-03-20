8e333d40883727d55bf0ba19422edc06b28811a7
ilyapatis24 <ilyapatis24@mail.ru>
2024-03-20 23:21:09 +0300

    first commit

---
+++ 01/Square_func.h
@@ -0,0 +1,27 @@
+﻿#pragma once
+#include <iostream>
+#include <vector>
+
+template<typename T>
+T square(T& number1) {
+	return number1 * number1;
+}
+
+template<typename V>
+void square(std::vector<V>& vect) {
+	for (V& elem : vect) {
+		elem *= elem;
+	}
+ }
+
+template<typename V>
+void print_vector(std::vector<V>& vect) {
+	for (size_t i = 0; i < vect.size(); i++) {
+		if (i == (vect.size()-1)) {
+			std::cout << vect[i];
+		}
+		else std::cout << vect[i] << ", ";
+	};
+}
+
+
---
+++ 01/main.cpp
@@ -0,0 +1,17 @@
+﻿#include "Square_func.h"
+
+int main(int argc, char** argv) {
+    setlocale(LC_ALL, "Russian");
+    auto number1 = 0;
+    std::vector<int> vect{ 1,2,3 };
+    std::cout << "[IN]: ";
+    std::cin >> number1;
+    std::cout << "[OUT]: " << square(number1) << "\n";
+    std::cout << "[IN]: ";
+    print_vector(vect);
+    std::cout << std::endl;
+    std::cout << "[OUT]: ";
+    square(vect);
+    print_vector(vect);
+    return 0;
+}
